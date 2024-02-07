1707c193fff47e220a84d04856ca0a048583d2b5
Kaleen1 <65292014+Kaleen1@users.noreply.github.com>
2024-02-07 11:13:03 +0000

    Add Food.cs Class

---
+++ Food.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssessmentShop
+{
+    public class Food
+    {
+        // Attributes
+        private string name, genre, picture;
+        private int price;
+
+        // Getter Methods
+        public string getName()
+        {
+            return name; 
+        }
+
+        public int getPrice()
+        {
+            return price;
+        }
+
+        public string getGenre()
+        {
+            return genre;
+        }
+
+        public string getPicture()
+        {
+            return picture;
+        }
+
+
+    }
+}
